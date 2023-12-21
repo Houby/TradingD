8acd81993b16fd62f7816fa68c9b15c2299e661c
Houby <bykoffmitya0412@yandex.ru>
2023-12-21 23:31:13 +0300

    set leverage + cancel all orders

--- basic_actions.py
+++ basic_actions.py
@@ -28,3 +28,35 @@ def market_price(symbol, category='linear'):
     dataParsed = json.loads(response.text)
     lastMarketPrice = dataParsed['result']['list'][0]['lastPrice']
     return lastMarketPrice
+
+# отмена всех ордеров по тикеру
+def cancel_all_order(symbol, category='linear'):
+    url = 'https://api.bybit.com/v5/order/cancel-all'
+    current_time = int(time.time() * 1000)
+    data = '{' + f'"symbol": "{symbol}", "category": "{category}"' + '}'
+    sign = hashing(str(current_time) + api_key + '5000' + data)
+
+    headers = {
+        'X-BAPI-API-KEY': api_key,
+        'X-BAPI-TIMESTAMP': str(current_time),
+        'X-BAPI-SIGN': sign,
+        'X-BAPI-RECV-WINDOW': str(5000)
+    }
+
+    response = requests.post(url=url, headers=headers, data=data)
+
+# установка плечей
+def set_leverage(symbol, buyLeverage, sellLeverage, category='linear'):
+    url = 'https://api.bybit.com/v5/position/set-leverage'
+    current_time = int(time.time() * 1000)
+    data = '{' + f'"symbol": "{symbol}", "buyLeverage": "{buyLeverage}", "sellLeverage": "{sellLeverage}", "category": "{category}"' + '}'
+    sign = hashing(str(current_time) + api_key + '5000' + data)
+
+    headers = {
+        'X-BAPI-API-KEY': api_key,
+        'X-BAPI-TIMESTAMP': str(current_time),
+        'X-BAPI-SIGN': sign,
+        'X-BAPI-RECV-WINDOW': str(5000)
+    }
+
+    response = requests.post(url=url, headers=headers, data=data)
